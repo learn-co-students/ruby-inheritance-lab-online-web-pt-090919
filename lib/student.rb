937e806666c0083aaf6dd42e03d5d63a38bd2512
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-14 16:34:58 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require "pry"
+class Student < User
 
+  def initialize
+    @knowledge = []
+
+  end
+
+  def learn(x)
+    @knowledge << x
+  end
+
+  def knowledge
+    @knowledge
+  end
+#binding.pry
 end
